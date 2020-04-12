8dda7f9b41d694ac1284a3d7f1eac25c3d6e5adc
chenxy <chenxy@tingyun.com>
2020-04-12 23:51:44 +0800

    关闭 刷新

--- layuiadmin/layui/arrange/attender2rule/attender2rule.js
+++ layuiadmin/layui/arrange/attender2rule/attender2rule.js
@@ -310,7 +310,7 @@ layui.config({
 			},
 			refresh: function(){
 				// console.log("========");
-				// alert("----");
+				alert("----");
 					location.reload();
 			},
 			dels: function() {
@@ -400,7 +400,7 @@ layui.config({
 				parent.layer.close(index); //再执行关闭 
 			}
 		};
-	$('.test-table-operate-btn .layui-btn,.test-table-operate-btn .layui-ds,.test-table-reload-btn .layui-allcheck').on('click', function() {
+	$('.layui-icon-refresh-3,.icon-wodehuiyi,.test-table-operate-btn .layui-btn,.test-table-operate-btn .layui-ds,.test-table-reload-btn .layui-allcheck').on('click', function() {
 		var type = $(this).data('type');
 		active[type] ? active[type].call(this) : '';
 	});
