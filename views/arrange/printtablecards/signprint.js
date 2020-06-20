cc14b90c38fc4a747429be980eb2cc2955733484
chenxy <chenxy@tingyun.com>
2020-06-20 21:30:34 +0800

    手动打印

--- views/arrange/printtablecards/signprint.js
+++ views/arrange/printtablecards/signprint.js
@@ -12,30 +12,123 @@ layui.config({
     var url = setter.baseUrl;
     // var url = "http://127.0.0.1:8083";
     
-    var namestr = sessionStorage.getItem("_printnames") || "";
-    console.log("namestr",namestr)
-    if(namestr){
-        $.ajax({
-            url: url+"/printseatsgin/printSeatsginname?name=" + namestr,
-            type: "get",
-            async: false,
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function(obj) {
-                console.log("printSeatsginname-----",data);
 
-                if(obj.code == 0){
-                    var data = obj.data || [];
-                    $('#name').val(data.join(''));
-                }
-            },
-            error:function(){
-                layer.closeAll();
-            }
+    $("#print").bind('click',function(){
+        $("#container").jqprint({
+            debug: false, 
+            importCSS: true, 
+            printContainer: true, 
+            operaSupport: false
         });
+    });
+
+    $("#smallbtn").bind('click',function(){
+        setSeatsign();
+    });
+
+    function setSeatsign(){
+        var data = {};
+        data.id = 123;
+        data.isgrouplist = 3
+        layer.open({
+            type: 2,
+            title: '设置',
+            area: ['50%', '40%'],
+            btn: ['确定', '取消'],
+            maxmin: true,
+            content: 'setup_pop.html',
+            success: function(layero, index) {
+                
+            },
+            yes:function (index,layero) {
+                var sign = sessionStorage.getItem("__selectsign") || "";
+                console.log("sign-----",sign);
+
+                if(sign){
+                    showName(JSON.parse(sign));
+                }
+
+                layer.close(index);
+                // var submit = layero.find('iframe').contents().find("#click");
+                // submit.click();
+            }
+        })
     }
 
+    
+    function showName(styles){
+        // var namestr = sessionStorage.getItem("_printnames") || "";
+        var textarea = document.getElementById("name");
+        var value = textarea.value;
+        var names = value.split("\n");
+
+        if(names){
+            var html = [];
+
+            var namestyle = "";
+            var style = "width:" + styles.width + "mm;height:" + styles.length + "mm;"
+            var classtext = "preview_left_text " + styles.font;
+            if(styles.position || styles.align){
+                classtext += " flex ";
+                if(styles.align){
+                    classtext += (" " + styles.align);
+                }
+                if(styles.position){
+                    classtext += (" " + styles.position);
+                }
+            }
+
+            namestyle = " font-size:"+styles.fontSize + "mm;letter-spacing:"+styles.vertical + "mm;"
+            if(styles.above || styles.left){
+                namestyle += " margin-top:"+styles.above + "mm;margin-left:"+styles.left + "mm;"
+            }
+            // $("#printnametext").css({
+            //     "font-size":(+seatSignData.fontSize/fontcc)+"mm",
+            //     "letter-spacing":(+domlevel)+"mm",
+            //     "margin-top":(+domabove)+"mm",
+            //     "margin-left":(+domleft)+"mm"
+            // });
+
+            //0:一字两面,1:一字一面, 一字两面 就是 一正一反， 一字一面，就是两个一样
+            //transform: rotate(180deg);
+            var type = +styles.type; 
+
+            for(var i = 0, len = names.length; i < len; i++){
+
+                var name = names[i] || {};
+                if(type == 0){
+                    html.push('<div>');
+                    html.push('<div id="fontwh" class="' + classtext + '" style="transform: rotate(180deg); ' + style + '">');
+                    html.push('<span id="printnametext" class="printname" style="' + namestyle + '">' + name + '</span>');
+                    html.push('</div>');
+                    html.push('</div>');
+
+                    html.push('<div>');
+                    html.push('<div id="fontwh" class="' + classtext + '" style="' + style + '">');
+                    html.push('<span id="printnametext" class="printname" style="' + namestyle + '">' + name + '</span>');
+                    html.push('</div>');
+                    html.push('</div>');
+                }else{
+                    html.push('<div>');
+                    html.push('<div id="fontwh" class="' + classtext + '" style="' + style + '">');
+                    html.push('<span id="printnametext" class="printname" style="' + namestyle + '">' + name + '</span>');
+                    html.push('</div>');
+                    html.push('</div>');
+                    
+                    
+                    html.push('<div>');
+                    html.push('<div id="fontwh" class="' + classtext + '" style="' + style + '">');
+                    html.push('<span id="printnametext" class="printname" style="' + namestyle + '">' + name + '</span>');
+                    html.push('</div>');
+                    html.push('</div>');
+                }
+            }
+            $("#container").html(html.join(''));
+
+        }
+
+        // control+p
+    }
 
 
 
