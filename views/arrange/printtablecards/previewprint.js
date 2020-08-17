ac86d33caa12d2f5fdeae8da072202a5c012112e
chenxy <chenxy@tingyun.com>
2020-08-18 00:14:58 +0800

    add

--- views/arrange/printtablecards/previewprint.js
+++ views/arrange/printtablecards/previewprint.js
@@ -29,7 +29,6 @@ layui.config({
             withCredentials: true
         },
         success: function(obj) {
-            debugger
             console.log("findseatsginByid-----",obj);
             if(obj.code == 0){
                 var styles = obj.data || {};
