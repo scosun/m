0a128bd097f5de24955df65b4dc3d25cc67f04fc
chenxy <chenxy@tingyun.com>
2021-03-06 14:22:50 +0800

    pad

--- pad/js/signcontrol.js
+++ pad/js/signcontrol.js
@@ -226,7 +226,7 @@ SignControl.prototype = {
             
             ws.onmessage = function (evt) {
                 var received_msg = evt.data || "";
-
+                
                 // console.log("onmessage-----",received_msg)
                 t.meetWebSocketMessage(received_msg);
                 
@@ -265,14 +265,21 @@ SignControl.prototype = {
         }
     },
     meetWebSocketMessage(received_msg){
+        console.log("数据已接收...",received_msg);
+
+        if(this.backComplete == 1){
+            return;
+        }
+
         var t = this;
 
-        t.backComplete = 0;
+        // t.backComplete = 0;
+
 
         // if(typeof H5JsMeeting != "undefined"){
         //     H5JsMeeting.showTipsDialog("ws----onmessage-----"+received_msg);
         // }
-        console.log("数据已接收...",received_msg);
+        
 
 
         // 会议状态
@@ -361,12 +368,12 @@ SignControl.prototype = {
 
     appTableSignControl:function(type){
         // -1.默认 可以点， 会议准备， 设备重启
-        // 1.会议准备完成，可以点 会议开始，重启会议，设备重置
-        // 2.会议开始完成，可以点 会议暂停，会议结束，重启会议，设备重置
-        // 3.会议暂停完成，可以点 会议恢复，会议结束，重启会议，设备重置
-        // 4.会议恢复完成，可以点 会议暂停，会议结束，重启会议，设备重置
-        // 5.会议结束完成，可以点 会议开始，重启会议，设备重置
-        // 6.会议重启完成，可以点 会议开始，重启会议，设备重置
+        // 1.会议准备完成，可以点 会议准备，会议开始，重启会议，设备重置
+        // 2.会议开始完成，可以点 开始完成，会议暂停，会议结束，重启会议，设备重置
+        // 3.会议暂停完成，可以点 会议暂停，会议恢复，会议结束，重启会议，设备重置
+        // 4.会议恢复完成，可以点 会议暂停，会议恢复，会议结束，重启会议，设备重置
+        // 5.会议结束完成，可以点 会议准备，会议结束，重启会议，设备重置
+        // 6.会议重启完成，可以点 会议准备，重启会议，设备重置
         // 7.重置桌牌完成，可以点 会议准备，设备重置
 
         // if(typeof H5JsMeeting != "undefined"){
@@ -374,6 +381,42 @@ SignControl.prototype = {
         // }
 
         if(this.backComplete == 0){
+            if(this.meetingType === 1){
+                // if(!(type === 1 || type === 2 || type === 6 || type === 7)){
+                    this.showStatusTip("会议准备中");
+                    return;
+                // }
+            }else if(this.meetingType === 2){
+                // if(!(type === 2 || type === 3 || type === 5 || type === 6 || type === 7)){
+                    this.showStatusTip("会议开始中");
+                    return;
+                // }
+            }else if(this.meetingType === 3){
+                // if(!(type === 3 || type === 4 || type === 5 || type === 6 || type === 7)){
+                    this.showStatusTip("会议暂停中");
+                    return;
+                // }
+            }else if(this.meetingType === 4){
+                // if(!(type === 3 || type === 4 || type === 5 || type === 6 || type === 7)){
+                    this.showStatusTip("会议恢复中");
+                    return;
+                // }
+            }else if(this.meetingType === 5){
+                // if(!(type === 1 || type === 5 || type === 6 || type === 7)){
+                    this.showStatusTip("会议结束中");
+                    return;
+                // }
+            }else if(this.meetingType === 6){
+                // if(!(type === 1 || type === 6 || type === 7)){
+                    this.showStatusTip("会议重启中");
+                    return;
+                // }
+            }else if(this.meetingType === 7){
+                // if(!(type === 1 || type === 7)){
+                    this.showStatusTip("桌牌重启中");
+                    return;
+                // }
+            }
             return;
         }
 
@@ -382,38 +425,31 @@ SignControl.prototype = {
                 return;
             }
         }else if(this.meetingType === 1){
-            if(!(type === 2 || type === 6 || type === 7)){
-                this.showStatusTip("会议准备中");
+            if(!(type === 1 || type === 2 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 2){
-            if(!(type === 3 || type === 5 || type === 6 || type === 7)){
-                this.showStatusTip("会议开始中");
+            if(!(type === 2 || type === 3 || type === 5 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 3){
-            if(!(type === 4 || type === 5 || type === 6 || type === 7)){
-                this.showStatusTip("会议暂停中");
+            if(!(type === 3 || type === 4 || type === 5 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 4){
-            if(!(type === 3 || type === 5 || type === 6 || type === 7)){
-                this.showStatusTip("会议恢复中");
+            if(!(type === 3 || type === 4 || type === 5 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 5){
-            if(!(type === 2 || type === 6 || type === 7)){
-                this.showStatusTip("会议结束中");
+            if(!(type === 1 || type === 5 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 6){
-            if(!(type === 2 || type === 6 || type === 7)){
-                this.showStatusTip("会议重启中");
+            if(!(type === 1 || type === 6 || type === 7)){
                 return;
             }
         }else if(this.meetingType === 7){
             if(!(type === 1 || type === 7)){
-                this.showStatusTip("桌牌重启中");
                 return;
             }
         }
@@ -437,6 +473,8 @@ SignControl.prototype = {
         //     H5JsMeeting.showTipsDialog("type--ajax--------------"+type);
         // }
         
+        this.backComplete = 0;
+
         $.ajax({
             async: true,
             type: "post",
@@ -449,8 +487,9 @@ SignControl.prototype = {
             },
             //失败的回调函数
             error: function() {
+                this.backComplete = 1;
                 console.log("error")
-            }
+            }.bind(this)
         });
     },
 
@@ -488,26 +527,26 @@ SignControl.prototype = {
                 obj.status = 1;
             }
             var meetingState = +obj.meetingState;
-            if(meetingState == 3 || meetingState == -3){
-                this.meetingType = 0;
-                this.readyMeeting(obj);
-            }else if(meetingState == 1 || meetingState == -1){
+            if(meetingState == 1 || meetingState == -1){
                 this.meetingType = 1;
-                this.startMeeting(obj);
-            }else if(meetingState == 4 || meetingState == -4){
-                this.meetingType = 2;
-                this.stopMeeting(obj);
-            }else if(meetingState == 5 || meetingState == -5){
-                this.meetingType = 3;
-                this.restoreMeeting(obj);
+                this.readyMeeting(obj);
             }else if(meetingState == 2 || meetingState == -2){
+                this.meetingType = 2;
+                this.startMeeting(obj);
+            }else if(meetingState == 3 || meetingState == -3){
+                this.meetingType = 3;
+                this.stopMeeting(obj);
+            }else if(meetingState == 4 || meetingState == -4){
                 this.meetingType = 4;
+                this.restoreMeeting(obj);
+            }else if(meetingState == 5 || meetingState == -5){
+                this.meetingType = 5;
                 this.finishMeeting(obj);
             }else if(meetingState == 6 || meetingState == -6){
-                this.meetingType = 5;
+                this.meetingType = 6;
                 this.rebootMeeting(obj);
             }else if(meetingState == 7 || meetingState == -7){
-                this.meetingType = 6;
+                this.meetingType = 7;
                 this.resetMeeting(obj);
             }
         }.bind(this));
@@ -628,11 +667,11 @@ SignControl.prototype = {
         }
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -1){
+        // if(meetingStatus == -1){
             html.push('<div class="b">');
             html.push('<a id="status_0" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -649,11 +688,11 @@ SignControl.prototype = {
         }
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -2){
+        // if(meetingStatus == -2){
             html.push('<div class="b">');
             html.push('<a id="status_1" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -671,11 +710,11 @@ SignControl.prototype = {
         // html.push('<p>会议暂停：<span>' + (meetingStatus == 0 ? '未发送' : '') +'</span></p>');
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -3){
+        // if(meetingStatus == -3){
             html.push('<div class="b">');
             html.push('<a id="status_2" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -693,11 +732,11 @@ SignControl.prototype = {
         // html.push('<p>会议恢复：<span class="r">' + (meetingStatus == 0 ? '未发送' : '') +'</span></p>');
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -4){
+        // if(meetingStatus == -4){
             html.push('<div class="b">');
             html.push('<a id="status_3" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -715,11 +754,11 @@ SignControl.prototype = {
         // html.push('<p>会议结束：<span>' + (meetingStatus == 0 ? '未发送' : '') +'</span></p>');
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -5){
+        // if(meetingStatus == -5){
             html.push('<div class="b">');
             html.push('<a id="status_4" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -737,11 +776,11 @@ SignControl.prototype = {
         // html.push('<p>会议重启：<span class="r">' + (meetingStatus == 0 ? '未发送' : '') +'</span></p>');
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -6){
+        // if(meetingStatus == -6){
             html.push('<div class="b">');
             html.push('<a id="status_5" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('<li>');
@@ -759,11 +798,11 @@ SignControl.prototype = {
         // html.push('<p>设备重置：<span>' + (meetingStatus == 0 ? '未发送' : '') +'</span></p>');
         html.push('<p>2020-12-16 16:51:53</p>');
         html.push('</div>');
-        if(meetingStatus == -7){
+        // if(meetingStatus == -7){
             html.push('<div class="b">');
             html.push('<a id="status_6" href="javascript:void(0);" class="popcontent-btn">重发</a>');
             html.push('</div>');
-        }
+        // }
         html.push('</div>');
         html.push('</li>');
         html.push('</ul>');
@@ -780,31 +819,32 @@ SignControl.prototype = {
     sendOne(evt){
         var id = evt.currentTarget.id;
         // console.log(this,id);
-        if(this.meetingStatus == -1 && id == "status_0"){
+        if(id == "status_0"){
             //单发 会议准备
             console.log("status_0");
             this.sendOneNewTableSign(1);
-        }else if(this.meetingStatus == -2 && id == "status_1"){
+        }else if(id == "status_1"){
             //单发 会议开始
             console.log("status_1");
             this.sendOneNewTableSign(2);
-        }else if(this.meetingStatus == -3 && id == "status_2"){
+        }else if(id == "status_2"){
             //单发 会议暂停
             console.log("status_2");
             this.sendOneNewTableSign(3);
-        }else if(this.meetingStatus == -4 && id == "status_3"){
+        }else if(id == "status_3"){
             //单发 会议恢复
             console.log("status_3");
             this.sendOneNewTableSign(4);
-        }else if(this.meetingStatus == -5 && id == "status_4"){
+        }else if(id == "status_4"){
             //单发 会议结束
             console.log("status_4");
             this.sendOneNewTableSign(5);
-        }else if(this.meetingStatus == -6 && id == "status_5"){
+        }else if(id == "status_5"){
             //单发 会议重启
             console.log("status_5");
             this.sendOneNewTableSign(6);
-        }else if(this.meetingStatus == -7 && id == "status_6"){
+        }else if(id == "status_6"){
+            // this.meetingStatus == -7 && 
             //单发 会议重置
             console.log("status_6");
             this.sendOneNewTableSign(7);
